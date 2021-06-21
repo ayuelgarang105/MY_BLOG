6bc48faf193e2c6c71b79cb11dd8749c29c60dc7
gabriel <ayuelgarang105@gmail.com>
2021-06-21 18:27:27 +0300

    Changed the email adress

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 export API_BASE_URL='http://quotes.stormconsultancy.co.uk/random.json'
-export MAIL_USERNAME='benardakaka484@gmail.com'
-export MAIL_PASSWORD ='20302000b'
-export SECRET_KEY='bena'
+
+export MAIL_USERNAME='gabriel.ayuel@student.moringaschool.com'
+export MAIL_PASSWORD='gazajoga@Moringa'
+export SECRET_KEY='ayuel'
 
 python3.8 manage.py server 
